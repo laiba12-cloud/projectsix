34a24c62f1ba3595d093194f36946a42a30b6854
laiba12-cloud <laibayounis123@gmail.com>
2025-04-26 13:48:53 +0500

    I make modifications

---
+++ facebookscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class FaceBookScreen extends StatefulWidget {
+  const FaceBookScreen({super.key});
+
+  @override
+  State<FaceBookScreen> createState() => _FaceBookScreenState();
+}
+
+class _FaceBookScreenState extends State<FaceBookScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+body:Center(
+  child:Text('Facebbok',),
+),
+
+    );
+  }
+}
---
+++ lib/facebbokscreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class FaceBookScreen extends StatefulWidget {
+  const FaceBookScreen({super.key});
+
+  @override
+  State<FaceBookScreen> createState() => _FaceBookScreenState();
+}
+
+class _FaceBookScreenState extends State<FaceBookScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child:Text('hiiii',),
+      ),
+    );
+  }
+}
